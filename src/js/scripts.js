a52b28cc245cf7512698dad2d23494e55f758647
criar-sites <criarisites1@gmail.com>
2026-06-27 14:24:35 -0300

    start 1

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+    link.addEventListener('click',function(e){
+        e.preventDefault();
+
+        const alvo = document.querySelector(this.getAttribute('href'));
+
+        alvo.scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
